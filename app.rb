8e59c98f989c93f9bcdab00d20bdd8d108f63ece
bry4n <bryan@ffiirree.com>
2009-11-21 19:19:09 -0600

    test

--- app.rb
+++ app.rb
@@ -1,28 +1,27 @@
 require "yaml"
-
-config = YAML.load_file("configs/config.yml")
-if config["logging"] == true
-  log = File.new('log/sinatra.log', 'a+')
-  $stderr.reopen(log)
-end
-
 require 'rubygems'
 require 'sinatra'
 require 'configs/deps'
 
-set :environment, :"#{config['environment']}"
-set :server, config["server"]
-set :host, config["host"]
-set :port, config["port"].to_i
-set :views, 'app/views'
-set :public, 'public'
+class MVC < Sinatra::Base
+  @config = YAML.load_file("configs/config.yml")
+  if @config["logging"] == true
+    log = File.new('log/sinatra.log', 'a+')
+    $stderr.reopen(log)
+  end
+  load 'configs/configures.rb'
+  set :environment, :"#{@config['environment']}"
+  set :server, @config["server"]
+  set :host, @config["host"]
+  set :port, @config["port"].to_i
+  set :views, 'app/views'
+  set :public, 'public'
+  enable :sessions, :logging, :dump_errors, :raise_errors, :static
 
-enable :sessions, :logging, :dump_errors, :raise_errors, :static
+  Dir.glob("app/controllers/*.rb") {|file| load file}
+  Dir.glob("app/models/*.rb") {|file| load file}
+  Dir.glob("helpers/*.rb") {|file| load file}
 
-load 'configs/configures.rb'
+  load "configs/routes.rb"
 
-Dir.glob("app/controllers/*.rb") {|file| load file}
-Dir.glob("app/models/*.rb") {|file| load file}
-Dir.glob("helpers/*.rb") {|file| load file}
-
-load "configs/routes.rb"
+end
--- config.ru
+++ config.ru
@@ -2,4 +2,4 @@ require 'app'
 
 use Rack::ShowExceptions
 
-run Sinatra::Application
+MVC.run!
