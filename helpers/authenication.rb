9b8ab3ab7d87488eef5ba05545140369564646b5
bry4n <bryan@ffiirree.com>
2009-11-21 20:38:08 -0600

    renamed auth file

---
+++ helpers/authenication.rb
@@ -0,0 +1,36 @@
+module Sinatra
+
+  module Authenication
+    
+    def logout!
+      session.delete :authorized_user
+    end
+
+    def authenticated?
+      session[:authorized_user]
+    end
+
+    def authenticate!
+      session[:authorized_user] = true
+    end
+
+    def authorize!(redirect="/denied")
+      redirect(redirect) unless authenticated?
+    end
+
+    get "/denied" do
+       "Access denied!"
+    end
+
+    post "/denied" do
+       "Access denied!"
+    end
+
+    alias_method :no_guest!, :authorize!
+    alias_method :acts_as_member?, :authenticated?
+    alias_method :bye!, :logout!
+
+  end
+
+  helpers Authenication
+end
